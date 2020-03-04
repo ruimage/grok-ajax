cc6f4c3d61f37207a7e61ebbdf0a8e4bc4621e3c
ehpc <ehpc@ehpc.io>
2020-03-04 09:47:00 +0300

    query-as-string

--- answers/puzzle7/server.js
+++ answers/puzzle7/server.js
@@ -7,7 +7,8 @@ const app = express();
 // Роут, отвечающий на запрос GET /query-as-string
 app.get('/query-as-string', (req, res) => {
   // Отправляем в ответ query-строку
-  return res.send(req.url.replace(/.+(?=\?)/));
+  const questionIndex = req.url.indexOf('?');
+  return res.send(questionIndex === -1 ? '' : req.url.slice(questionIndex));
 });
 
 // Запуск сервера по порту 3000
