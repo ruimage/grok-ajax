db9936f9e42574ce5519b08f17baf27887e4e145
ehpc <ehpc@ehpc.io>
2020-03-06 12:20:36 +0300

    Add style to public

---
+++ tests/puzzle300.test.js
@@ -0,0 +1,27 @@
+describe('Puzzle 300', () => {
+  global.testSanity('puzzle300');
+  describe('Исходный код', () => {
+    it('использует шаблонизатор', async () => {
+      const src = await global.getSource('puzzle300', 'server.js');
+      expect(src).toContain('express.static');
+      expect(src).toContain('public');
+    });
+    it('стиль корректный', async () => {
+      const src = await global.getSource('puzzle300', 'public/style.css');
+      expect(src).toContain('font-size');
+      expect(src).toContain('72pt');
+      expect(src).toContain('body');
+    });
+    it('шаблон index.hbs корректный', async () => {
+      const src = await global.getSource('puzzle300', 'views/index.hbs');
+      expect(src).toEqual('Привет!');
+    });
+  });
+  describe('Роут GET /', () => {
+    it('выводит нужный текст', async () => {
+      const res = await global.fetch(`${global.url}/`);
+      expect(res).toContain('Привет!');
+    });
+  });
+  afterAll(() => global.puzzle300.kill());
+});
